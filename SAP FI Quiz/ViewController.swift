02f004e5ca74543e515a8740465f3f57cd0c8f1c
Viktor Varsano <Viktor_varsano@hotmail.com>
2020-02-18 13:18:28 +0200

    Initial Commit

---
+++ SAP FI Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAP FI Quiz
+//
+//  Created by Viktor Varsano on 18.02.20.
+//  Copyright © 2020 Viktor Varsano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
